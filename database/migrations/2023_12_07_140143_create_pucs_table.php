ded3d371df1352aa830d3d316adc55883fce7314
Diego Andres Quevedo Perez <diego01andres@hotmail.com>
2024-07-31 15:57:43 -0500

    Ajuste Tabla PUC

--- database/migrations/2023_12_07_140143_create_pucs_table.php
+++ database/migrations/2023_12_07_140143_create_pucs_table.php
@@ -13,19 +13,17 @@ return new class extends Migration
     {
         Schema::create('pucs', function (Blueprint $table) {
             $table->id();
-            $table->string('puc',10)->unique();
-            $table->string('grupo',1);
+            $table->string('puc', 10)->unique();
+            $table->string('grupo', 1);
             $table->string('descripcion');
-            $table->string('nivel' );
-            $table->string('naturaleza',1);
+            $table->string('nivel');
+            $table->string('naturaleza', 1);
             $table->boolean('mayor_rep')->default(false);
             $table->boolean('movimiento')->default(false);
             $table->boolean('subcentro')->default(false);
             $table->boolean('bancaria')->default(false);
             $table->boolean('tercero')->default(false);
-            $table->string('puc_padre',10);
-            $table->unsignedBigInteger('pucs_id')->nullable();
-            $table->foreign('pucs_id')->on('pucs')->references('id');
+            $table->string('puc_padre', 10);
             $table->boolean('base_gravable')->default(false);
             $table->boolean('mueve_modulo')->default(false);
             $table->boolean('codigo_dian')->default(false);
