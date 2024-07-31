4db6eba96b0d6587ab498599ca3fe9498ee15a06
Diego Andres Quevedo Perez <diego01andres@hotmail.com>
2024-07-31 14:41:20 -0500

    Lista PUC Padre

--- app/Filament/Resources/PucResource.php
+++ app/Filament/Resources/PucResource.php
@@ -57,9 +57,8 @@ class PucResource extends Resource
                     ->maxLength(1)
                     ->required()
                     ->label('Nivel Cuenta'),
-                Select::make('puc_padre')
+                TextInput::make('puc_padre')
                     ->label('PUC Padre')
-                    ->searchable()
                     ->native(false)
                     ->getSearchResultsUsing(fn (string $search): array => Puc::where('puc_padre', '=', "{$search}")->orWhere('descripcion', 'like', "%{$search}%")->limit(50)->pluck('descripcion', 'id')->toArray())
                     ->getOptionLabelUsing(fn ($value): ?string => Puc::find($value)?->puc),
