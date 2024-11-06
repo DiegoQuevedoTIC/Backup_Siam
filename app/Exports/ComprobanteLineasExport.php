9f898868db4f23e35fa7de1b86a4dc1949983374
Alberto Godoy <76492268+albertogodoy@users.noreply.github.com>
2024-11-06 09:13:37 -0400

    se agrega columna tercero al exportar las lineas de un comprobante

--- app/Exports/ComprobanteLineasExport.php
+++ app/Exports/ComprobanteLineasExport.php
@@ -17,16 +17,19 @@ class ComprobanteLineasExport implements FromQuery, WithHeadings
     {
         $this->comprobante = $comprobante;
     }
-
     public function query()
     {
-        return ComprobanteLinea::query()->where('comprobante_id', $this->comprobante)->join('pucs as p', 'pucs_id', 'p.id')->select('p.puc', 'descripcion_linea', 'debito', 'credito', 'linea', 'BASE_GRAVABLE', 'CHEQUE');
+        return ComprobanteLinea::query()->where('comprobante_id', $this->comprobante)
+            ->join('pucs as p', 'pucs_id', 'p.id')
+            ->join('terceros as t', 'comprobante_lineas.tercero_id', 't.id')
+            ->select('p.puc', 't.tercero_id as tercero', 'descripcion_linea', 'debito', 'credito', 'linea', 'BASE_GRAVABLE', 'CHEQUE');
     }
 
     public function headings(): array
     {
         return [
             'PUC',
+            'TERCERO',
             'DESCRIPCION',
             'DEBITO',
             'CREDITO',
