653f2e8db82e6d3e5ea1d617a5e0f0ed9c2bba8b
Alberto Godoy <76492268+albertogodoy@users.noreply.github.com>
2024-11-07 22:11:27 -0400

    se ajusta ingreso con correo verificado

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,6 +52,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@fondep.com') && $this->hasVerifiedEmail();
+        return $this->hasVerifiedEmail();
     }
 }
